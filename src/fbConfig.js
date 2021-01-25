5eb4f84d2db622a3d5a3eca8d5dfbbc50a989150
bharath-vallala <38830389+bharath-vallala@users.noreply.github.com>
2021-01-25 17:07:44 +0530

    Update fbConfig.js

--- src/fbConfig.js
+++ src/fbConfig.js
@@ -4,12 +4,12 @@
 let firebaseConfig={}
   
   firebaseConfig = {
-        apiKey: "AIzaSyAh8F9MCHsphK5BQ1b8fvFssXJQ_FSy-Lw",
-        authDomain: "moneymanagerr.herokuapp.com",
-        projectId: "moneymanage-2068d",
-        storageBucket: "moneymanage-2068d.appspot.com",
-        messagingSenderId: "249274745904",
-        appId: "1:249274745904:web:c4a233305082eb8d8d59f5"
+        apiKey: "",
+        authDomain: "",
+        projectId: "",
+        storageBucket: "",
+        messagingSenderId: "",
+        appId: ""
       };
 
 
